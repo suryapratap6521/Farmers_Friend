2d70cdc6c3fcc27872b1921bab4adc155082953e
Shivam Kumar <life.shivam2394@gmail.com>
2024-04-15 23:04:57 +0530

    test

--- src/components/Core/Auth/PrivateRoute.jsx
+++ src/components/Core/Auth/PrivateRoute.jsx
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom'
 export default function PrivateRoute({ children }) {
 
   const { token } = useSelector((state) => state.auth);
+  // delete comment
   // const {user}=useSelector((state)=>state.profile);
 
   if (token !== null) {
