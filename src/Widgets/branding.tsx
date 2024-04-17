622c6ec254d7f15fb80e0694266033a9cb010d93
Shivam Kumar <life.shivam2394@gmail.com>
2024-04-17 12:55:39 +0530

    Final Commit

--- src/Widgets/branding.tsx
+++ src/Widgets/branding.tsx
@@ -3,45 +3,57 @@ import Marquee from 'react-fast-marquee';
 
 // Images
 
-import first from "../assets/first.png";
-import three from "../assets/3.jpg";
-import fourth from "../assets/4.jpg";
-import fifth from "../assets/5.jpg";
-import six from "../assets/6.jpg";
-import first1 from "../assets/first.jpg";
-import seven from "../assets/7.jpg";
-import eight from "../assets/8.jpg";
-import nine from "../assets/9.jpg";
-import ten from "../assets/10.jpeg";
+// import three from "../assets/3.jpg";
+// import fourth from "../assets/4.jpg";
+// import fifth from "../assets/5.jpg";
+// import six from "../assets/6.jpg";
+// import first1 from "../assets/first.jpg";
+// import seven from "../assets/7.jpg";
+// import eight from "../assets/8.jpg";
+// import nine from "../assets/9.jpg";
+// import ten from "../assets/10.jpeg";
 
+import first from "../assets/first.png";
+import two from '../assets/marque/2.png';
+import three from '../assets/marque/3.png';
+import four from '../assets/marque/4.png';
+import five from '../assets/marque/5.png';
+import six from '../assets/marque/6.png';
+import one from '../assets/marque/1.png';
+import seventh from '../assets/marque/7.png';
+import eight from '../assets/marque/8.png';
+import nine from '../assets/marque/9.png';
 
 export const partners = [
     {
         url: first,
     },
     {
+        url: four,
+    },
+    {
+        url: two,
+    },
+    {
         url: three,
-    },
-    {
-        url: fourth,
-    },
-    {
-        url: fifth,
-    },
-    {
-        url: six,
-    },
-    {
-        url: seven,
-    },
-    {
-        url: eight,
     },
     {
         url: nine,
     },
     {
-        url: ten,
+        url: five,
+    },
+    {
+        url: six,
+    },
+    {
+        url: seventh,
+    },
+    {
+        url: eight,
+    },
+    {
+        url: one,
     },
 ];
 
